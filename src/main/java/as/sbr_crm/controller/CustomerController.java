1d9500b181096664c7d740dcc8bc051add3e01f3
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2021-01-27 21:04:31 +0530

    added functionality to add new records

--- src/main/java/as/sbr_crm/controller/CustomerController.java
+++ src/main/java/as/sbr_crm/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import as.sbr_crm.entity.Customer;
 import as.sbr_crm.service.CustomerService;
@@ -24,5 +26,23 @@ public class CustomerController
 		model.addAttribute("customers", customers);
 		
 		return "clist";
+	}
+	
+	@GetMapping("/add")
+	public String AddCustomer(Model model)
+	{
+		Customer customer = new Customer();
+		
+		model.addAttribute("customer", customer);
+		
+		return "cform";
+	}
+	
+	@PostMapping("/save")
+	public String SaveCustomer(@ModelAttribute("customer") Customer customer)
+	{
+		customerService.SaveCustomer(customer);
+		
+		return "redirect:/";
 	}
 }
