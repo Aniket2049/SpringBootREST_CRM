65b5cfda968e6671634408fd9270cf032c9d2129
Aniket Singh <76955108+Aniket2049@users.noreply.github.com>
2021-01-27 22:36:43 +0530

    added Put mapping with updating functionality

--- src/main/java/as/sbr_crm/rest/CustomerRESTController.java
+++ src/main/java/as/sbr_crm/rest/CustomerRESTController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,15 @@ public class CustomerRESTController
 	}
 	
 	@PostMapping("/customers")
-	public Customer SaveEmployee(@RequestBody Customer customer)
+	public Customer SaveCustomer(@RequestBody Customer customer)
+	{
+		customerService.SaveCustomer(customer);
+		
+		return customer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer UpdateCustomer(@RequestBody Customer customer)
 	{
 		customerService.SaveCustomer(customer);
 		
